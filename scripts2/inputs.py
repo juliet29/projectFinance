7a0fa64f07c284363758f60d9d6fa13b9eaafd9e
Juliet Nwagwu Ume-Ezeoke <jnwagwu@stanford.edu>
2023-02-25 14:07:19 -0800

    inputs script

--- scripts2/inputs.py
+++ scripts2/inputs.py
@@ -1,5 +1,35 @@
 from collections import OrderedDict
 import pandas as pd
-import numpy as np 
+from data import * 
 
-# pandas, make a multindex dataframe, with the name of the dictionary 
+def make_two_index_df(d_orig):
+    d = d_orig.copy()
+    # make entries into arrays
+    for inner_dict in d.values():
+        for k, v in inner_dict.items():
+            inner_dict[k] = [v]
+    # arrange the dictionary to have tuples 
+    reform_d = {(outerKey, innerKey): values for outerKey, innerDict in d.items() for innerKey, values in innerDict.items()}
+    # make the multi index dictionary 
+    df = pd.DataFrame(reform_d, index=["Values"])
+    df = df.T
+    return df
+
+# ! Construction Phase 
+d = {
+    "Pre-Financial Close Expenses": pre_fc_costs,
+    "Financial Close Expenses": fc_costs,
+    "Construction Period Even Split Monthly Expenses": monthly_split_costs,
+    "Construction Period Custom Schedule Monthly Expenses": epc_data
+}
+const_phase_inputs = make_two_index_df(d)
+
+# ! Operations Phase 
+oper_d = {
+    "Commisioning Fees": comm_fees,
+    "Operating Fees to External Entities": other_fees
+}
+oper_phase_inputs = make_two_index_df(oper_d)
+
+# ! Corporate Phase
+corp_inputs = make_two_index_df(corporate_costs)
